b36ddaec9b5f6df26f0b94a3f7868656a1f09e42
GeraLavadenz <riomahabramromamateo@gmail.com>
2024-10-19 12:36:40 -0400

    app function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 function App() {
-  
+  const [cards, setcards] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getCard();
+        setcards(response);
+        console.log(cards);
+      } catch (error) {
+        console.log("Error al pedir");
+      }
+    }
+  }, []);
 
   return (
     <>
-      
+      <section>
+        {cards.map((card) => (
+          <Card key={card.id} card={card} />
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
